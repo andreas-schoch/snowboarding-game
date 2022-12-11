fa939f07b4546ac43f9c99049b456570d5c2e8e1
Andreas Schoch <andreas_schoch@outlook.com>
2022-12-11 23:40:56 +0100

    v1.0.0: fixes for MVP (without leaderboard)

--- src/src/scenes/GameScene.ts
+++ src/src/scenes/GameScene.ts
@@ -19,21 +19,27 @@ export default class GameScene extends Ph.Scene {
   }
 
   private create() {
-    this.cameras.main.setDeadzone(50, 125);
+    this.cameras.main.setDeadzone(50 / 2, 125 / 2);
     this.cameras.main.setBackgroundColor(0x555555);
     const resolutionMod = this.cameras.main.width / DEFAULT_WIDTH;
     this.cameras.main.setZoom(DEFAULT_ZOOM * resolutionMod);
     this.cameras.main.scrollX -= this.cameras.main.width / 2;
     this.cameras.main.scrollY -= this.cameras.main.height / 2;
 
-    this.b2Physics = new Physics(this, 40, new Pl.b2Vec2(0, -10));
+    // FIXME the world size is supposed to be set to 40px per 1m but due to floating point precision issues
+    //  it is currently halfed and zoom is doubled temporarily. Visually it looks the same but needs to be fixed.
+    //  The issue is that the terrain is a single object instead of chunked and gets weird once player moves too far from the origin.
+    //  This wasn't an issue when terrain was procedural and chunked, so will likely fix itself once that is optimized again.
+    this.b2Physics = new Physics(this, 20, new Pl.b2Vec2(0, -10));
     this.playerController = new PlayerController(this, this.b2Physics);
     this.terrain = new Terrain(this, this.b2Physics);
 
     this.cameras.main.startFollow(this.b2Physics.rubeLoader.getBodiesByCustomProperty('bool', 'phaserCameraFollow', true)[0].GetUserData() as Phaser.GameObjects.Image, false, 0.8, 0.25);
-    this.cameras.main.followOffset.set(-375, 0);
-
-    this.scene.launch(GameUIScene.name, [this.observer, () => this.scene.restart()]);
+    this.cameras.main.followOffset.set(-375 / 2, 0);
+    this.scene.launch(GameUIScene.name, [this.observer, () => {
+      this.playerController.state.reset();
+      this.scene.restart();
+    }]);
 
     this.backdrop = new Backdrop(this);
 
@@ -54,7 +60,7 @@ export default class GameScene extends Ph.Scene {
     graphics.setDepth(1000);
     graphics.strokePath();
 
-    this.observer.on('enter-crashed', () => {
+    this.observer.on('enter_crashed', () => {
       this.cameras.main.shake(200, 0.01);
       this.b2Physics.enterBulletTime(-1, 0.4);
     });
