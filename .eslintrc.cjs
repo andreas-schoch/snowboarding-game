2299e7d6fea42648b1dd155be281c39c0638aac5
= <andreas_schoch@outlook.com>
2024-01-16 06:18:58 +0100

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,52 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:solid/typescript'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'solid'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    // 'linebreak-style': [
+    //     'error',
+    //     'windows',
+    //     'unix'
+    // ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
