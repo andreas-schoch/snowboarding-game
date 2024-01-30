6f17e133f5b0a6f464240bc9addca0af8f23a549
= <andreas_schoch@outlook.com>
2024-01-30 22:40:50 +0100

    editor: create EditorController class

---
+++ game/src/controllers/EditorController.ts
@@ -0,0 +1,49 @@
+import {DEFAULT_WIDTH} from '..';
+import {Settings} from '../Settings';
+import {EditorScene} from '../scenes/EditorScene';
+
+export class EditorController {
+  controls: Phaser.Cameras.Controls.SmoothedKeyControl;
+  resolutionMod: number;
+
+  pointerX: number | null = null;
+  pointerY: number | null = null;
+
+  pointerCoordX: number | null = null;
+  pointerCoordY: number;
+
+  constructor(private scene: EditorScene) {
+    this.setupCameraAndInput();
+  }
+
+  update(time: number, delta: number) {
+    this.controls.update(delta);
+  }
+
+  private setupCameraAndInput() {
+    const camera = this.scene.cameras.main;
+    this.resolutionMod = camera.width / DEFAULT_WIDTH;
+    camera.setZoom(Settings.defaultZoom() * this.resolutionMod);
+    camera.setBackgroundColor(0x333333);
+    camera.centerOnX(0);
+    camera.centerOnY(0);
+
+    const keyboard = this.scene.input.keyboard;
+    if (!keyboard) throw new Error('cursors is null');
+    this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl({
+      camera,
+      up: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+      left: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+      down: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+      right: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+      zoomIn: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q),
+      zoomOut: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
+      acceleration: 10,
+      drag: 0.1,
+      maxSpeed: 1,
+      maxZoom: 2,
+      minZoom: 0.3, // whenever this is adjusted, BackdropGrid also needs to be adjusted for now 
+      zoomSpeed: 0.02,
+    });
+  }
+}
--- game/src/scenes/EditorScene.ts
+++ game/src/scenes/EditorScene.ts
@@ -1,25 +1,17 @@
-import {DEFAULT_WIDTH, SCENE_EDITOR} from '..';
+import {SCENE_EDITOR} from '..';
 import {Backdrop} from '../Backdrop';
 import {BackdropGrid} from '../BackdropGrid';
 import {Settings} from '../Settings';
-import { drawCoordZeroPoint } from '../helpers/drawCoordZeroPoint';
+import {EditorController} from '../controllers/EditorController';
+import {drawCoordZeroPoint} from '../helpers/drawCoordZeroPoint';
 import {Physics} from '../physics/Physics';
 import {RubeScene} from '../physics/RUBE/RubeLoaderInterfaces';
 
 export class EditorScene extends Phaser.Scene {
-  observer: Phaser.Events.EventEmitter = new Phaser.Events.EventEmitter();
-  controls!: Phaser.Cameras.Controls.SmoothedKeyControl;
-
-  pointerX: number | null = null;
-  pointerY: number | null = null;
-
-  pointerCoordX: number | null = null;
-  pointerCoordY: number;
-  resolutionMod: number;
-
   b2Physics: Physics;
   private backdrop: Backdrop;
   private backdropGrid: BackdropGrid;
+  private controller: EditorController;
 
   constructor() {
     super({key: SCENE_EDITOR});
@@ -31,53 +23,22 @@ export class EditorScene extends Phaser.Scene {
   }
 
   private create() {
-    this.setupCameraAndInput();
     this.backdrop = new Backdrop(this);
     this.backdropGrid = new BackdropGrid(this);
     this.b2Physics = new Physics(this, {worldScale: 40, gravityX: 0, gravityY: -10});
+    this.controller = new EditorController(this);
 
     drawCoordZeroPoint(this);
 
+    // TODO ability to load individual RubeScenes and treat them as an Object Entity similar like in RUBE
+    //  An object may contain many Bodies, Joints, Fixtures, etc. but will be displayed as a single entity
     const rubeScene: RubeScene = this.cache.json.get(Settings.selectedCharacter());
     this.b2Physics.load(rubeScene, 0, 0);
   }
 
   update(time: number, delta: number) {
-    this.controls.update(delta);
+    this.controller.update(time, delta);
     this.backdrop.update();
     this.backdropGrid.update();
-  }
-
-  private setupCameraAndInput() {
-    // CAMERA STUFF
-    const camera = this.cameras.main;
-    this.resolutionMod = camera.width / DEFAULT_WIDTH;
-    camera.setZoom(Settings.defaultZoom() * this.resolutionMod);
-    camera.setBackgroundColor(0x333333);
-    camera.centerOnX(0);
-    camera.centerOnY(0);
-    // KEYBOARD STUFF
-    const keyboard = this.input.keyboard;
-    if (!keyboard) throw new Error('cursors is null');
-    const keyW = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    const keyA = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    const keyS = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    const keyD = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-
-    this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl({
-      camera,
-      up: keyW,
-      left: keyA,
-      down: keyS,
-      right: keyD,
-      zoomIn: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q),
-      zoomOut: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
-      acceleration: 10,
-      drag: 0.1,
-      maxSpeed: 1,
-      maxZoom: 2,
-      minZoom: 0.3, // whenever this is adjusted, BackdropGrid also needs to be adjusted for now 
-      zoomSpeed: 0.02,
-    });
   }
 }
