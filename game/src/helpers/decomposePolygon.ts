f1e72a26c4e58771acd83062725bf74bddb3cd3f
= <andreas_schoch@outlook.com>
2024-02-29 10:02:27 +0100

    editor: revert decomposePolygon() to initial version as new one isn't reliable

--- game/src/helpers/decomposePolygon.ts
+++ game/src/helpers/decomposePolygon.ts
@@ -10,33 +10,39 @@ export function decomposePolygon(vertices: RubeVectorArray): RubeVectorArray[] {
   console.time('decomposePolygon');
   const polygon = fromVectorArrayToPolygon(vertices);
 
-  if (!isSimple(polygon)) throw new Error('Polygon cannot intersect itself');
-
   makeCCW(polygon);
-  // removeCollinearPoints(polygon, 0.05);
-  // removeDuplicatePoints(polygon, 0.005);
-  const convexPolygons = quickDecomp(polygon); // Ensures there are no concave polygons
+  const convexPolygons = quickDecomp(polygon); // Make sure there are no concave polygons
 
-  const validPolygons: Polygon[] = [];
+  const newConvexPolygons: Polygon[] = [];
   for (const polygon of convexPolygons) {
+    // If the polygon has 8 or less vertices, we can use it as is
     if (polygon.length <= 8) {
-      validPolygons.push(polygon);
+      newConvexPolygons.push(polygon);
       continue;
     }
 
     // split it until we have only polygons with 8 or less vertices
-    for (let i = 0; i < polygon.length; i += 7) {
-      const end = Math.min(i + 8, polygon.length);
-      const sliced = polygon.slice(i, end);
-      validPolygons.push(sliced);
+    for (let i = 0; i < polygon.length; i += 7) { // Use 7 to ensure overlap and closure
+      if (i + 8 < polygon.length) {
+        const sliced = polygon.slice(i, i + 8);
+        newConvexPolygons.push(sliced);
+      } else {
+        // Handle last slice differently to ensure it includes the first vertex for closure
+        const sliced = polygon.slice(i, polygon.length);
+        // Ensure the polygon is closed by adding the starting point if not already included
+        if (sliced[0] !== sliced[sliced.length - 1]) {
+          sliced.push(polygon[0]);
+        }
+        newConvexPolygons.push(sliced);
+        break; // we've reached the end
+      }
     }
   }
 
-  const result = validPolygons.map(fromPolygonToVectorArray);
+  const result = newConvexPolygons.map(fromPolygonToVectorArray);
   console.timeEnd('decomposePolygon');
   return result;
 }
-
 function fromVectorArrayToPolygon(polygon: RubeVectorArray): Polygon {
   const result: Polygon = [];
   for (let i = 0; i < polygon.x.length; i++) {
